1af22393a2439ab3a1aa3738c019dcb54e7b98b3
ieunji0116 <136577730+ieunji0116@users.noreply.github.com>
2024-10-22 13:12:51 +0900

    Update Home.js

--- front/src/pages/Home/Home.js
+++ front/src/pages/Home/Home.js
@@ -8,21 +8,18 @@ function Home() {
       <section className="hero">
         <h1 className="hero-title">ProjectEJ</h1>
         <p className="hero-description">
-          무슨말무슨말무슨말무슨말무슨말무슨말무슨말무슨말
+        classification 이미지 분류
         </p>
       </section>
       <section className="features">
-        <Link to="/" className="feature">
-          <h2 className="feature-title">첫번째</h2>
-          <p className="feature-description">무슨말무슨말무슨말무슨말무슨말</p>
-        </Link>
+
         <Link to="/about" className="feature">
-          <h2 className="feature-title">두번째</h2>
-          <p className="feature-description">무슨말무슨말무슨말무슨말무슨말</p>
+          <h2 className="feature-title">classification1</h2>
+          <p className="feature-description">classification1</p>
         </Link>
         <Link to="/upload" className="feature">
-          <h2 className="feature-title">세번째</h2>
-          <p className="feature-description">무슨말무슨말무슨말무슨말무슨말</p>
+          <h2 className="feature-title">classification2</h2>
+          <p className="feature-description">classification2</p>
         </Link>
       </section>
     </div>
